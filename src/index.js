f07beaca6947b085a36174a8d62cc18df2625c25
PolinaMoiseyenko <pmoiseyenko@gmail.com>
2018-09-24 01:14:45 +0300

    Love-triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+// your implementation
+var count = 0;
+for(let i = 0; i < preferences.length; i++)
+{
+  let firstLover = preferences[i];
+  let secondLover = preferences[firstLover];
+  let thirdLover = preferences[secondLover];
+  if (thirdLover == i) 
+  {
+    count++;
+  }
+}
+return count / 3;
 };
